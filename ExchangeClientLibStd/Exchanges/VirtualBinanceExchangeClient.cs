ea6fd6df5011fd4121e0b0958eb81b22225a9c6b
Michael Delpach <McoreD@users.noreply.github.com>
2020-12-23 06:28:00 +0800

    Upgrade csproj to net5

---
+++ ExchangeClientLibStd/Exchanges/VirtualBinanceExchangeClient.cs
@@ -0,0 +1,9 @@
+﻿namespace ExchangeClientLib
+{
+    public class VirtualBinanceExchangeClient : ExchangeClient
+    {
+        public VirtualBinanceExchangeClient(string apiKey, string secretKey) : base(apiKey, secretKey)
+        {
+        }
+    }
+}
